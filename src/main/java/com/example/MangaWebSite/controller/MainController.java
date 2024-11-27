456bfe5f73a8ca98f5468a63ab97d2482aad9bbc
Sasha <laplata228@gmail.com>
2024-11-27 20:37:36 +0200

    new TODO

--- src/main/java/com/example/MangaWebSite/controller/MainController.java
+++ src/main/java/com/example/MangaWebSite/controller/MainController.java
@@ -74,6 +74,7 @@ public class MainController {
         //Нові комікси
         model.addAttribute("newComics", comicsService.getNewCreatedComics(0).getContent());
         model.addAttribute("bookmarkedComics", chapterService.getNewChaptersInTabs(0));
+        //TODO Зробити щоб вони також грузились як нові коміксі
         return "main";
     }
     @GetMapping("/comics/new")
