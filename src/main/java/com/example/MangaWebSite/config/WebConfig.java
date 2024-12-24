45c6b79ec0f1457537fa395857d3babbc2487444
Sasha <laplata228@gmail.com>
2024-12-24 20:14:41 +0200

    Виправи таймліф

--- src/main/java/com/example/MangaWebSite/config/WebConfig.java
+++ src/main/java/com/example/MangaWebSite/config/WebConfig.java
@@ -1,8 +1,10 @@
 package com.example.MangaWebSite.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -10,5 +12,12 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("file:C:/uploads/");
+    }
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:/templates/");
+        resolver.setSuffix(".html");
+        return resolver;
     }
 }
