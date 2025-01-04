877ba4cfa05f883eb545bdd57b5bacdb71270722
Sasha <laplata228@gmail.com>
2025-01-04 23:25:15 +0200

    добавив хедер в "Зараз читають", залишилось доробити пару стилів(добавити іконки в селект і змінити трошки стиль фотографій)

---
+++ src/main/java/com/example/MangaWebSite/controller/GlobalControllerAdvice.java
@@ -0,0 +1,17 @@
+package com.example.MangaWebSite.controller;
+
+import com.example.MangaWebSite.models.Person;
+import com.example.MangaWebSite.security.PersonDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class GlobalControllerAdvice {
+
+    @ModelAttribute("person")
+    public Person getGlobalPerson(@AuthenticationPrincipal PersonDetails personDetails) {
+        return personDetails != null ? personDetails.getPerson() : null;
+    }
+}
